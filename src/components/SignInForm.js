a077a70686c173c84b5df4a89b582b832f8ed659
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-08-20 16:33:23 -0400

    sign in form

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import {useForm} from 'react-hook-form'
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-const SignInForm = (props) => {
-    const { register, handleSubmit} = useForm();
+export default function App() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+  
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+      <input type="password" placeholder="Password" {...register("Password", {})} />
 
-    return (
-    <form onSubmit={handleSubmit(data => saveData(data))}>
-        <h1>Sign In</h1>
-        <label>Username</label>
-        <input username="username" ref={register({ required: "Required"})}/>
-        <label>Password</label>
-        <input password="password" ref={register({ required: "Required"})}/>
-        <input type="submit" className="submitButton"/>
+      <input type="submit" />
     </form>
-    )
+  );
 }
-
-export default SignInForm
