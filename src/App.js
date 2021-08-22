5814581f6ae33efaccc91d91e2a8e32121f7d86a
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-08-22 11:37:14 -0400

    logo component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import SignInForm from './components/SignInForm';
-import SignUpForm from './components/SignUpForm'
+import SignUpForm from './components/SignUpForm';
+import Logo from './components/Logo';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>#adulting is hard</h1>
       <SignInForm />
       <SignUpForm />
+      <Logo />
     </div>
   );
 }
