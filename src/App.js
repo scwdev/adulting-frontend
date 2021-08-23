5fed989e0f0dc4078fe49230dd58b20e4fd93d26
Chris <Chris@margaritas-air.attlocal.net>
2021-08-23 09:00:17 -0700

    crudtodo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,56 @@ import SignInForm from './components/SignInForm';
 import SignUpForm from './components/SignUpForm'
 
 function App() {
+
+const url = "https://adulting-backend.herokuapp.com";
+const [todo, setTodo] = React.useState([]);
+
+// GET
+const getTodo = () => {
+  fetch(url + "/todo")
+  .then((response) => response.json())
+  .then((data) => {
+    setTodo(data);
+  });
+};
+
+React.useEffect(() => getTodo(), []);
+
+// CREATE
+const handleCreate = (newTodo) => {
+  fetch(url + "/todo", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newTodo),
+  }).then(() => {
+    getTodo();
+  });
+};
+
+// UPDATE
+const handleUpdate = (todo) => {
+  fetch(url + "/todo/" + todo._id, {
+    method: "put",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(todo),
+  }).then(() => {
+    getTodo();
+  });
+};
+
+// DELETE
+const deleteWeed = (todo) => {
+  fetch(url + "/todo/" + todo._id, {
+    method: "delete",
+  }).then(() => {
+    getTodo();
+  });
+};
+  
   return (
     <div className="App">
       <h1>#adulting is hard</h1>
