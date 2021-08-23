88898489735cc7488f216618e48772501531bfba
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-08-23 19:27:25 -0400

    small things

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import Logo from './Logo'
 
 const Nav = (props) => {
     return (
         <div>
+            <Logo />
             <Link to="/mylist">My List</Link>
             <Link to="/task/:id">One Thing</Link>            
             <Link to="/newtask">New Task</Link>
