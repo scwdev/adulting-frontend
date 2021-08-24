4d47a52479bc21ff60606f772125d6f1fdf441fa
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-08-24 16:45:39 -0400

    home page styling

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Logo from '../components/Logo'
+import '../styles/home.scss'
 
 
 
 
 const Homepage = (props) => {
     return (
-        <div>
-            <h1>Homepage</h1>
-            <Link to="/new"><button>Add Tasks</button></Link>
-            <Link to="/mylist"><button>Task List</button></Link>
-            <Link to="task/:id"><button>One Task</button></Link>
-        </div>
+        <div className="home">
+            <Logo lo="homeLogo"/> 
+            <h1>Make <span>#adulting</span> easier!</h1>
+            <div className="homeButtonContainer">
+            <Link to="/new"><button className="addTasksButton">Add a New Task</button></Link>
+            <Link to="/mylist"><button className="taskListButton">Your Task List</button></Link>
+            <Link to="task/:id"><button className="oneTaskButton">If you only have time for one thing...</button></Link>
+            </div>
+    </div>
     )
 }
 
