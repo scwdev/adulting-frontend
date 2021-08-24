0d5f8ce65dea6d533067abf7cfe82a666c243c6c
Noam Blanks <noamblanks@gmail.com>
2021-08-23 23:28:18 -0400

    task list populates. got carried away doing other things.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ const Nav = (props) => {
         <div>
             <Link to="/mylist">My List</Link>
             <Link to="/task/:id">One Thing</Link>            
-            <Link to="/newtask">New Task</Link>
+            <Link to="/new">New Task</Link>
             <Link to="/logout">Logout</Link>
         </div>
     )
--- src/pages/TaskList.js
+++ src/pages/TaskList.js
@@ -1,12 +1,36 @@
 import React from "react"
+import { Link } from 'react-router-dom'
 
 import Nav from "../components/Nav"
 
+import { prioritySort } from "../functions/prioritySort"
+
 const TaskList = (props) => {
+
+    const tasks = props.tasks
+
+    const loading = () => ("loading...")
+    const loaded = () => {
+        const resetTimer = (input) => {
+            input = {...input, lastDone: Date.now()}
+            props.handleUpdate(input)
+        }
+        return (
+            prioritySort(tasks).map((item,index) => (
+                <li>
+                    <Link to={`/task/${item._id}`}>{item.name}</Link>
+                    <button onClick={() => {resetTimer(item)}}>Did it!</button>
+                </li>                
+            ))
+        )
+    }
+
     return (
         <div>
             <Nav/>
-            "Task List Page"
+            <ul>
+                {Array.isArray(tasks) === true ? loaded() : loading()}
+            </ul>
         </div>
     )
 }
