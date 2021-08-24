a32f3ee0e27bdd2035d4d5e6cf67c2c5c464b0be
Noam Blanks <noamblanks@gmail.com>
2021-08-24 12:25:28 -0400

    edit page auto populates in progress. fixed useEffect

--- src/pages/AddEdit.js
+++ src/pages/AddEdit.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 
 import Nav from '../components/Nav';
@@ -7,13 +7,33 @@ const AddEdit = (props) => {
   const [ initial, setInitial ] = useState("")
   const { register, handleSubmit, formState: { errors } } = useForm();
 
-  if (props.match.params.id) {
-    console.log{'ding!'}
-  }
+  useEffect(() => {
+    if (props.match.params.id) {
+      const data = props.tasks.filter((item) => (item._id === props.match.params.id))
+      const freq = data[0].frequency
+      switch (true) {
+        case freq%365 === 0:
+          data[0].frequency = {number: freq/365, multiplier: 365}
+          break;
+        case freq%28  === 0:
+          data[0].frequency = {number: freq/28, multiplier: 28}
+          break;
+        case freq%7 === 0:
+          data[0].frequency = {number: freq/7, multiplier: 7}
+          break;
+        default:
+          data[0].frequency = {number: freq, multiplier: 1} 
+      }
+      setInitial(data[0])
+    } else {
+      setInitial("")
+    }
+  }, [props.tasks])
 
   // console.log(errors);
   const addEdit = (data) => {
     //TODO "new Date(data.lastDone)" returns date off by 4 hours. needs a fix
+    console.log(data.lastDone)
     const lastDone = data.lastDone === "" ? new Date() : Date.parse(data.lastDone)
     data = {
       ...data,
@@ -30,13 +50,13 @@ const AddEdit = (props) => {
       <Nav />
       <form onSubmit={handleSubmit(addEdit)}>
         <label>I want to
-          <input type="text" placeholder="do a thing" {...register("name", {required: true})} />
+          <input type="text" defaultValue={initial.name} placeholder="do a thing" {...register("name", {required: true})} />
         </label>
         <br />
         <label>
           Every 
-          <input type="number" placeholder="0" {...register("frequency.number", {required: true})} />
-          <select {...register("frequency.multiplier")}>
+          <input type="number" defaultValue={initial?.frequency?.number} placeholder="42" {...register("frequency.number", {required: true})} />
+          <select defaultValue={initial?.frequency?.multiplier} {...register("frequency.multiplier")}>
             <option value="1">Days</option>
             <option value="7">Weeks</option>
             <option value="28">Months</option>
@@ -45,7 +65,7 @@ const AddEdit = (props) => {
         </label>
         <br />
         <label for="lastDone">I last did that on
-          <input type="date" placeholder="datetime" {...register("lastDone", {})} />
+          <input type="date" defaultValue={Date.now()} placeholder="datetime" {...register("lastDone", {})} />
         </label>
         <br />
         <input type="submit" value="Set Reminder" />
