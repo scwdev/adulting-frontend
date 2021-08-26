4cdea3f357ac17f9ced1c6c27e789a185c8b06d5
Chris <Chris@margaritas-air.attlocal.net>
2021-08-26 08:04:42 -0700

    thumbtacks

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -15,7 +15,9 @@ const Homepage = (props) => {
             <Logo lo="homeLogo"/> 
             <h1>Make <span>#adulting</span> easier!</h1>
             <div className="homeButtonContainer">
-                <Link to="/new"><button className="addTasksButton">Add a New Task</button></Link>
+
+                <Link to="/new"><button className="addTasksButton"><span className="iconify" data-icon="fa:thumb-tack"></span>
+Add a New Task</button></Link>
                 <Link to="/mylist"><button className="taskListButton">Your Task List</button></Link>
                 <Link to={`task/${topId}`} ><button className="oneTaskButton">If you only have time for one thing...</button></Link>
             </div>
